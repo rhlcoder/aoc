c2692d5366e7531c55f968a327c46f831e831ca2
rhlcoder <rhlcoder@gmail.com>
2021-04-03 20:40:07 -0300

    Create read.go

---
+++ 2020/01/01/reading/read.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+)
+
+var ErrFileNotFound = errors.New("no esta el archivo")
+
+// SliceInput opens a text file and returns a slice containing its lines
+func SliceInput(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, ErrFileNotFound
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var slice = make([]string, 0)
+
+	for scanner.Scan() {
+		slice = append(slice, scanner.Text())
+	}
+
+	return slice, nil
+}
+
+// StringSliceToInt converts a slice of strings to int.
+func StringSliceToInt(s []string) []int {
+	var intSlice = make([]int, len(s))
+
+	for i, v := range s {
+		intSlice[i], _ = strconv.Atoi(v)
+	}
+
+	return intSlice
+}
