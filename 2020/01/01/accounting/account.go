12ce610799f510e99bc755cb97f962755929e892
rhlcoder <rhlcoder@gmail.com>
2021-04-05 21:36:24 -0300

    Finished part 2. A lot to refactor though

--- 2020/01/01/accounting/account.go
+++ 2020/01/01/accounting/account.go
@@ -4,7 +4,7 @@ import "errors"
 
 var ErrNoPairFound = errors.New("no pair of numbers satisfies the requirements")
 
-func SumObjetive(slice []int, objetive int) (x, y, iterations int, e error) {
+func SumPair(slice []int, objetive int) (x, y, iterations int, e error) {
 	l := len(slice)
 
 	for i := 0; i < l; i++ {
@@ -21,4 +21,25 @@ func SumObjetive(slice []int, objetive int) (x, y, iterations int, e error) {
 	}
 
 	return x, y, iterations, ErrNoPairFound
+}
+
+func SumTern(slice []int, objetive int) (x, y, z, iterations int, e error) {
+	l := len(slice)
+
+	for i := 0; i < l; i++ {
+		if slice[i] > objetive {
+			continue
+		}
+
+		for j := i + 1; j < l; j++ {
+			for k := j + 1; k < l; k++ {
+				if slice[i]+slice[j]+slice[k] == objetive {
+					return slice[i], slice[j], slice[k], iterations, nil
+				}
+				iterations++
+			}
+		}
+	}
+
+	return z, y, x, iterations, ErrNoPairFound
 }
