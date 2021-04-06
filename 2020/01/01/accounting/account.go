cd0eae83756f52b88776cb78f824c8910e43cb2a
rhlcoder <rhlcoder@gmail.com>
2021-04-05 21:48:21 -0300

    updated SumTern to improve iterations.
    Still a lot to refactor though

--- 2020/01/01/accounting/account.go
+++ 2020/01/01/accounting/account.go
@@ -32,6 +32,10 @@ func SumTern(slice []int, objetive int) (x, y, z, iterations int, e error) {
 		}
 
 		for j := i + 1; j < l; j++ {
+			if slice[i]+slice[j] > objetive { // minor adjustment that took 30x less iterations
+				continue
+			}
+
 			for k := j + 1; k < l; k++ {
 				if slice[i]+slice[j]+slice[k] == objetive {
 					return slice[i], slice[j], slice[k], iterations, nil
